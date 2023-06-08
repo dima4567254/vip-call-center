34f5396be8a1909ef2ccc4c145f22b720e668604
dima <dima.xipkov@gmail.com>
2023-06-09 02:22:17 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,53 @@
+
+
+
+
+    /*	$(".menu a").on("click", function (event) {
+    	    event.preventDefault();
+    	    var id = $(this).attr('href'),
+    	        top = $(id).offset().top;
+    	    $('body,html').animate({
+    	        scrollTop: top
+    	    }, 1500);
+    	});
+
+        /*для закрыть меню*/
+    /*
+              $('.menu__btn').on('click', function () {
+                  $('.menu__items').toggleClass('menu__items--active');
+              });
+              $('.menu__btn').on('click', function () {
+                  $('.menu__btn').toggleClass('active');
+              });
+           
+            $('.home__slider').slick({
+                dots: true,
+                arrows: false,
+                 responsive: [
+
+
+                     {
+                         breakpoint: 1150,
+                         settings: {
+                             slidesToShow: 4,
+                         }
+                     },
+                     {
+                         breakpoint: 1000,
+                         settings: {
+                             slidesToShow: 3,
+                         }
+                     },
+                     {
+                         breakpoint: 620,
+                         settings: {
+                             slidesToShow: 2,
+                             slidesToScroll: 2,
+                         }
+                     },
+                 ]
+            });
+           
+           
+           */
+
